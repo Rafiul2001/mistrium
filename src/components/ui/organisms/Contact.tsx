6f817adecd7d114284b5f5160e19af3a69d7577d
Shah Md. Rafiul Kadir <rafiul13062001@gmail.com>
2025-09-22 15:02:08 +0600

    contact and footer added

--- src/components/ui/organisms/Contact.tsx
+++ src/components/ui/organisms/Contact.tsx
@@ -1,26 +1,54 @@
-import { BsArrowRight } from "react-icons/bs"
-import Container from "../molecules/Container"
-import Flex from "../molecules/Flex"
-import Text from "../molecules/Text"
+import { BsArrowRight } from "react-icons/bs";
+import Container from "../molecules/Container";
+import Flex from "../molecules/Flex";
+import Text from "../molecules/Text";
+import ImageContainer from "../molecules/ImageContainer";
 
 const Contact: React.FC = () => {
-    return (
-        <Container className="pl-[108px] pr-[48px]">
-            <Flex className="flex-col">
-                <a href="#" className="block max-w-fit py-4 px-9 rounded-[30px] font-gupter font-bold tracking-[2%] border-[2px] text-primary border-primary">
-                    See Your Interior Design
-                </a>
-                <Text size={3} className="max-w-[349px] text-[30px] font-gupter font-bold leading-[39px] tracking-[2%] mt-6">Sign up fo the notification for add a new product’s</Text>
-                <Flex className="items-center">
-                    <Text className="ml-[26px] mr-[48px] my-[28px]">Your Busness Email</Text>
-                    <Flex>
-                        <Text>Get Started</Text>
-                        <BsArrowRight size={32} />
-                    </Flex>
-                </Flex>
+  return (
+    <Container className="pl-[108px] pr-[48px]">
+      <Flex className="items-end justify-between">
+        <Flex className="flex-col">
+          <a
+            href="#"
+            className="block max-w-fit py-4 px-9 rounded-[30px] font-gupter font-bold tracking-[2%] border-[2px] text-primary border-primary"
+          >
+            See Your Interior Design
+          </a>
+          <Text
+            size={3}
+            className="max-w-[349px] text-[30px] font-gupter font-bold leading-[39px] tracking-[2%] mt-6"
+          >
+            Sign up fo the notification for add a new product’s
+          </Text>
+          <Flex className="items-center mt-[16px] bg-[#D9D9D9]/50 w-fit pr-2 rounded-[13px]">
+            <Text className="ml-[26px] mr-[48px] my-[32px]">
+              Your Busness Email
+            </Text>
+            <Flex className="items-center">
+              <Text className="font-gupter h-auto font-bold text-[20px] tracking-[2%] text-white bg-[#C4A586] px-[42px] py-[23px] rounded-l-[13px]">
+                Get Started
+              </Text>
+              <div className="px-[27px] py-[22px] bg-[#C4A586]/44 rounded-r-[13px]">
+                <BsArrowRight size={32} />
+              </div>
             </Flex>
-        </Container>
-    )
-}
+          </Flex>
+        </Flex>
 
-export default Contact
+        <Flex className="gap-[9px] items-center max-w-fit">
+          <ImageContainer src="contact.png" alt="contact" />
+          <Text className="font-gupter font-medium text-[19px] tracking-[2%]">
+            WAtch Video’s
+          </Text>
+          <div>
+            <BsArrowRight size={32} />
+          </div>
+        </Flex>
+        <ImageContainer src="sign-up-sofa-image.png" alt="sign-up-sofa-image" />
+      </Flex>
+    </Container>
+  );
+};
+
+export default Contact;
--- src/components/ui/organisms/Footer.tsx
+++ src/components/ui/organisms/Footer.tsx
@@ -1,7 +1,71 @@
 import Container from "../molecules/Container";
+import Flex from "../molecules/Flex";
+import ImageContainer from "../molecules/ImageContainer";
+import NavItem from "../molecules/NavItem";
+import Text from "../molecules/Text";
+import NavMenu from "./NavMenu";
+import { FaFacebookF, FaYoutube } from "react-icons/fa";
+
+const navItems = [
+  {
+    id: 1,
+    name: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    name: "About Us",
+    href: "#",
+  },
+  {
+    id: 3,
+    name: "Products",
+    href: "#",
+  },
+  {
+    id: 4,
+    name: "FAQ'S",
+    href: "#",
+  },
+  {
+    id: 5,
+    name: "Contact Us",
+    href: "#",
+  },
+];
 
 const Footer: React.FC = () => {
-  return <Container>Footer</Container>;
+  return (
+    <div>
+      <Container className="pl-[108px] pr-[72px] mt-[114px]">
+        <Flex className="items-center justify-between">
+          <ImageContainer
+            className="max-w-[228px]"
+            src="Logo T.png"
+            alt="Logo T"
+          />
+          <NavMenu className="">
+            {navItems.map((item) => (
+              <NavItem
+                className="relative block after:absolute after:content-[''] after:top-full after:left-0 after:w-0 hover:after:w-full after:h-[3px] after:bg-black after:transition-all text-[17px] font-gupter tracking-[2%]"
+                key={item.id}
+                name={item.name}
+                href={item.href}
+              />
+            ))}
+          </NavMenu>
+          <Flex className="gap-4 items-center justify-end">
+            <FaYoutube size={24} fill="#B99272" />
+            <FaFacebookF size={24} fill="#B99272" />
+            <FaYoutube size={24} fill="#B99272" />
+            <FaFacebookF size={24} fill="#B99272" />
+          </Flex>
+        </Flex>
+      </Container>
+
+      <Text className="mt-[22px] pt-[47px] pb-[38px] text-center border-[2px] border-[#72696A]/39 font-gupter font-medium tracking-[2%]">All Copyright Reasurved By || <span className="text-[#C4A586]">ISTIAK</span></Text>
+    </div>
+  );
 };
 
 export default Footer;
